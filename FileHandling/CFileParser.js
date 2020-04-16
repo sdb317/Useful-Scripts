b5d1981f6b6f4749ebc68df4d621b2f32acf26c7
Simon <simon-bell@xl-direct.com>
2020-04-16 22:31:59 +0200

    Leverage Scripting.FileSystemObject for folders, files & records

---
+++ FileHandling/CFileParser.js
@@ -0,0 +1,101 @@
+///////////////////////////////////////////////////////////
+// File        : CFileParser.js
+// Created     : 01/18/12
+// Description : 
+//
+//    var FileParser=new CFileParser();
+//    FileParser.FileIterator
+//        (
+//        'C:\\Folder',
+//        function(FileName)
+//            {
+//            if (FileName.indexOf('.txt')!=-1) return true; else return false;
+//            },
+//        function(FileName)
+//            {
+//            FileParser.RecordIterator
+//                (
+//                'C:\\Folder\\'+FileName,
+//                function(RecordText)
+//                    {
+//                    if (RecordText.indexOf('Hello world!')!=-1) return true; else return false;
+//                    },
+//                function(RecordText)
+//                    {
+//                    Log(RecordText);
+//                    return true;
+//                    }
+//                );
+//            }
+//        );
+//
+//
+
+function CFileParser()
+{
+// Inheritance
+
+
+// Operations
+    this.FileIterator=_FileIterator;
+    function _FileIterator(PathName, Qualifies, Action) 
+        {
+        try
+            {
+            var Folder=FileSystem.GetFolder(PathName);
+			var FileList='';
+            var FilesCollection=new Enumerator(Folder.Files);
+            for (;!FilesCollection.atEnd();FilesCollection.moveNext())
+                {
+                var File=FilesCollection.item();
+                if (Qualifies(File.Name)) // Does the name match the criteria specified?
+                    {
+                    if (FileList.length>0) // Use a separator but not for the first
+						FileList+=';';
+					FileList+=File.Name;
+                    }
+                }
+            FileList=FileList.split(';');
+			FileList=FileList.sort();
+			for (var i=0;i<FileList.length;i++)
+				{
+                var FileName=FileList[i];
+                Action(FileName);
+				}
+            }
+        catch (e)
+            {
+            LogEvent('FileIterator: '+e.description);
+            }
+        }
+
+    this.RecordIterator=_RecordIterator;
+    function _RecordIterator(FileName, Qualifies, Action) 
+        {
+        try
+            {
+            var File=FileSystem.GetFile(FileName);
+            if (File!=null)
+                {
+                var Stream=File.OpenAsTextStream(1,-2);
+                while (!Stream.AtEndOfStream)
+                    {
+                    var RecordText=Stream.ReadLine();
+                    if (Qualifies(RecordText)) // Does the text match the criteria specified?
+                        {
+                        Action(RecordText);
+                        }
+                    }
+                Stream.Close();
+                }
+            }
+        catch (e)
+            {
+            LogEvent('RecordIterator: '+e.description);
+            }
+        }
+
+
+// Attributes
+
+};
---
+++ FileHandling/Lib.js
@@ -0,0 +1,44 @@
+FileSystem=new ActiveXObject('Scripting.FileSystemObject'); // Add this to the global namesapce
+
+function ReadText(FileName)
+    {
+    try 
+        {
+        var Stream=new ActiveXObject('ADODB.Stream');
+        Stream.Open();
+        Stream.Type=2;
+        Stream.Charset='UTF-8';
+        Stream.LoadFromFile(FileName);
+        var Text=Stream.ReadText();
+        Stream.Close();
+        return Text;
+        }
+    catch (e)
+        {
+        Log('ReadText failed: '+e.name);
+        }
+    return;
+    }
+
+function WriteText(Text,FileName)
+    {
+    try 
+        {
+        var Stream=new ActiveXObject('ADODB.Stream');
+        Stream.Open();
+        Stream.Type=2;
+        Stream.Charset='UTF-8';
+        Stream.WriteText(Text);
+        Stream.SaveToFile(FileName,2);
+        Stream.Close();
+        return true;
+        }
+    catch (e)
+        {
+        Log('WriteText failed: '+e.name);
+        }
+    return false;
+    }
+
+function crc32(r) {for(var a,o=[],c=0;c<256;c++){a=c;for(var f=0;f<8;f++)a=1&a?3988292384^a>>>1:a>>>1;o[c]=a}for(var n=-1,t=0;t<r.length;t++)n=n>>>8^o[255&(n^r.charCodeAt(t))];return(-1^n)>>>0}
+
